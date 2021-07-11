bd796870371911d5e5a3067357c8c24467fc1c53
Patrick Brennan <pbrennanmusic@gmail.com>
2021-07-11 17:04:26 -0400

    Added edit form to edit.erb, added book.category and book.genre expressions in show.erb. Created get/patch routes for edit function of app. Scrapped and re-migrated/re-seeded database to clear added dummy data.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   get '/books' do
+    @books = Book.all
+    @users = User.all
     erb :'books/index'
   end
 
@@ -23,4 +25,24 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     erb :'books/show'
   end
+
+  get '/books/:id/edit' do
+    @book = Book.find(params[:id])
+    erb :'books/edit'
+  end
+
+  patch '/books/:id' do
+    @book = Book.find(params[:id])
+    @book.title = params[:title] if !params[:title].empty?
+    @book.author = params[:author] if !params[:author].empty?
+    @book.year_of_publication = params[:year_of_publication] if !params[:year_of_publication].nil?
+    @book.category = params[:category] if !params[:category].empty?
+    @book.genre = params[:genre] if !params[:genre].empty?
+    @book.progress = params[:progress]
+    @book.user_rating = params[:user_rating]
+    @book.user_review = params[:user_review] if params[:user_review] != ""
+    @book.save
+    redirect "/books/#{@book.id}"
+  end
+
 end
